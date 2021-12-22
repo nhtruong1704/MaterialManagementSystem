c6fb3a0624b564f29f5d1608c1b3c7fac4745aa7
nhtruong1704 <dinhnhattruong211@gmail.com>
2021-12-22 20:10:27 +0300

    update

---
+++ app/src/main/java/com/example/materialmanagementsystem/EmployeeLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.materialmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_login);
+    }
+}
