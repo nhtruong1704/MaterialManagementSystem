01cf7b4feb541411c53e184a9a9650494de50f41
nhtruong1704 <dinhnhattruong211@gmail.com>
2021-12-26 02:49:10 +0300

    export material

--- app/src/main/java/com/example/materialmanagementsystem/AdminImportMaterialActivity.java
+++ app/src/main/java/com/example/materialmanagementsystem/AdminImportMaterialActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AdminAddMaterialActivity extends AppCompatActivity {
+public class AdminImportMaterialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_add_material);
+        setContentView(R.layout.activity_admin_import_material);
     }
 }
