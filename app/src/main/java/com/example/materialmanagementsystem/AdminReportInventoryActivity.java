e77439e89ee09e03642edf21af50e8510e35ed14
nhtruong1704 <dinhnhattruong211@gmail.com>
2021-12-26 23:17:24 +0300

    menu activity

---
+++ app/src/main/java/com/example/materialmanagementsystem/AdminReportInventoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.materialmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminReportInventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_report_inventory);
+    }
+}
--- app/src/main/java/com/example/materialmanagementsystem/AdminViewMaterialActivity.java
+++ app/src/main/java/com/example/materialmanagementsystem/AdminViewMaterialActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AdminInventoryActivity extends AppCompatActivity {
+public class AdminViewMaterialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
